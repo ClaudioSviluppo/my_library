2bea0c33691c55174b368fe1785eb08796b5588e
ClaudioSviluppo <76873902+ClaudioSviluppo@users.noreply.github.com>
2022-12-23 16:21:33 +0100

    Add SpeechToText dependency

--- lib/screens/book_screen.dart
+++ lib/screens/book_screen.dart
@@ -2,18 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../shared/menu_drawer.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
-class BookScreen extends StatelessWidget {
+class BookScreen extends StatefulWidget {
   const BookScreen({super.key});
+
+  @override
+  State<BookScreen> createState() => _BookScreenState();
+}
+
+class _BookScreenState extends State<BookScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(AppLocalizations.of(context)!.books),
-          backgroundColor: Colors.blueGrey,
-        ),
-        drawer: const MenuDrawer(),
-        body: const Center(child: FlutterLogo()));
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.books),
+        backgroundColor: Colors.blueGrey,
+      ),
+      drawer: const MenuDrawer(),
+      body: const Center(child: FlutterLogo()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.mic),
+      ),
+    );
   }
 }
